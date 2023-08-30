95de0863bc9fe23033aa5e783dc1925c8e7e5632
Efraim Fernandes <efraim.fernandes@outlook.com>
2023-08-29 23:58:49 -0300

    Desk 2: Sobre Mimestá finalizado... por hora

--- src/components/Desk2.tsx
+++ src/components/Desk2.tsx
@@ -1,9 +1,46 @@
 import styles from "../styles/Desk2.module.css"
+import imgSobreMim from "../../public/images/image-sobremim.png"
+import imgBall from "../../public/images/ball-sobremim.png"
+import Image from "next/image"
+import Nome from "./nome"
 
 export default function Desk2(){
     return (
         <div className={styles.Desk2} id="SobreMim">
-            <h1> Este é o Desk 2</h1>
+            <div className={styles.img}>
+                <Image 
+                    src={imgSobreMim}
+                    width={400}
+                    alt="Imagem Avatar..."
+                />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.cabecalho}>
+                    <h1>Sobre Mim</h1>
+                </div>
+                <div className={styles.text}>
+                    <p className={styles.textSobreMim}>
+                        Meu nome é {<Nome text="Efraim Quintunda Fernandes" />}
+                    </p> 
+                    <p className={styles.textSobreMim}>
+                        Antes de qualquer coisa, a senção de criar uma aplicação do iníco ao fim, 
+                        passando por cada etapa e vendo a evolução do todo, realmente não tem preço, 
+                        pode-se dizer que essa é uma das coisas que mais me fez ser tão apaixonado por programação! 
+                    </p>
+                    <p className={styles.textSobreMim}>
+                        Meus primeiros contatos com a progração foram em 2020, no meu primiero ano do ensino médio, porém, 
+                        só entrei de cabeça nesse mundo em 2023.Atualmente tenho um domíno avançado do front end  e um conhecimento rezoável no back-end. 
+                        Também gosto de criar designs no figma para  as interfaces dos meus projetos.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.imgBall}>
+                <Image 
+                    src={imgBall}
+                    width={300}
+                    alt="Imagem do semicículo no background"
+                />
+            </div>
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,9 @@
 import styles from "../styles/Navbar.module.css"
 import Image from "next/image"
 import EFlogo from "../../public/images/EF-logo.png"
-// import { useRef } from "react"
 import { Link } from "react-scroll"
 
 export default function Navbar() {
-
-//     const Inicio = useRef(null)
-//     const SobreMim = useRef(null)
-
-//     const scrollToDesk = (elementRef) => {
-//         window.scrollTo({
-//             top: elementRef.current.offsetTop,
-//             behavior: 'smooth'
-//         })
-//     }
-
     return (
         <div className={styles.navbar}>
             <Image
@@ -27,10 +15,10 @@ export default function Navbar() {
             />
             <div className={styles.navegacao}>
                 <li className={styles.navItem}>
-                    <Link to="Inicio" spy={true} smooth={true} offset={100} duration={500} className={styles.a}>Início</Link>
+                    <Link to="Inicio" spy={true} smooth={true} offset={-120} duration={500} className={styles.a}>Início</Link>
                 </li>
                 <li className={styles.navItem}>
-                    <Link to="SobreMim" spy={true} smooth={true} offset={100} duration={500} className={styles.a}>Sobre Mim</Link>
+                    <Link to="SobreMim" spy={true} smooth={true} offset={-150} duration={500} className={styles.a}>Sobre Mim</Link>
                 </li>
                 <li className={styles.navItem}>
                     <Link to="MinhasHabilidades" spy={true} smooth={true} offset={100} duration={500} className={styles.a}>Minhas Habilidades</Link>
