16959fb7c8ecbb6ea2a4bbfaa14ef26e2c767cb4
Efraim  Fernandes <efraim.fernandes@outlook.com>
2023-11-13 23:04:07 -0300

    Adicionando mais um projeto (linklibrary) e uma pequena alteração no cinemaven

--- src/components/Desk4.tsx
+++ src/components/Desk4.tsx
@@ -4,12 +4,16 @@ import styles from "../styles/Desk4.module.css"
 import { ProjectD, ProjectE} from "./ProjectModel"
 import Sneakers from "../../public/images/image-sneakers.png"
 import CineMaven from "../../public/images/image-cinemaven.png"
+import LinkLibrary from "../../public/images/image-linklibrary.png"
+
+
 
 export default function Desk4(){
     return (
         <div className={styles.Desk4} id="MeusProjetos">
             <h1 className={styles.titulo}>Veja alguns dos meus projetos...</h1>
             <div className={styles.projects}>
+                {/* Sneakers */}
                 <ProjectD
                     src={Sneakers}
                     alt="Imagem de um dos meus projetos, sneakers!"
@@ -19,7 +23,18 @@ export default function Desk4(){
                     gitProj="https://github.com/EfraimFernandes/sneakers"
                 />
 
+                {/* LinkLibrary */}
                 <ProjectE
+                    src={LinkLibrary}
+                    alt="Imagem de um dos meus projetos, LinkLibrary!"
+                    title="LinkLibrary"
+                    descProj="Aqui você poderá criar de forma prática e fácil uma biblioteca de links com todos que são importante para você"
+                    siteProj="https://linklibraryll.netlify.app"
+                    gitProj="https://github.com/EfraimFernandes/linklibrary"
+                />
+
+                {/* CineMaven */}
+                <ProjectD
                     src={CineMaven}
                     alt="Imagem de um dos meus projetos, CineMaven!"
                     title="CineMaven"
@@ -27,6 +42,8 @@ export default function Desk4(){
                     siteProj="https://cinemaven-efra.netlify.app"
                     gitProj="https://github.com/EfraimFernandes/CineMaven"
                 /> 
+
+                
             </div>
         </div>
     )
